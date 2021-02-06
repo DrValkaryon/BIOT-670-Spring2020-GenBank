2c15832b7c8961b1024e4c14b3f0cada028f42f6
DrValkaryon <57381290+DrValkaryon@users.noreply.github.com>
2021-02-06 14:12:07 -0500

    Minor Bug Fixes V1
    Fixed syntax issues, converted linear view to a python file from plain text.
    
    Removed record class reference because it referenced an undefined variable in the circular program.

--- LinearView.py
+++ LinearView.py
@@ -4,24 +4,24 @@
 
 from dna_features_viewer import BiopythonTranslator
 
-class CustomTranslator(BiophythonTranslator):
-
+class CustomTranslator(BiopythonTranslator):
     #To determine the features's label
-    label_areas = ["label", "note", "name", "gene"]
+    label_areas = ['label', 'note', 'name', 'gene']
 
     def compute_feature_legend (self, feature):
         return feature.type
 
     #hex colors for feature legend color
     def compute_feature_color (self, feature):
-        return {
-            "origin_replcation":  "1e794b", #green
+        color_map = {
+            "origin_replcation": "1e794b", #green
             "CDS":  "#f6870d", #orange
             "regulatory":  "ff0000", #red
             "misc_recomb":  "d1e9f1", #light blue
             "misc_feature":  "f886ff", #lavender
             "backbone":  "820068", #magenta
-            )[feature.type]
+        }
+        return color_map[feature.type]
 
     #background color for legend box
     def compute_feature_box_background(self, feature):
@@ -29,11 +29,12 @@ class CustomTranslator(BiophythonTranslator):
 
     def compute_feature_box_lindewidth(self, feature):
         return 0
+    
 
 translator = CustomTranslator()
-graphic-record = translator.translate_record("DNA.gb")
-ax, _ = graphic_record.plot(figure_width=15, strand_in_label_threshold=7)
-graphic_record.plot_legend(ax=ax, loc=1, ncol=3, frameon=False)
+grecord = translator.translate_record("DNA.gb")
+ax, _ = grecord.plot(figure_width=15, strand_in_label_threshold=7)
+grecord.plot_legend(ax=ax, loc=1, ncol=3, frameon=False)
 ax.figure.savefig("A_linear_graph.svg", bbox_inch="tight")
 
             

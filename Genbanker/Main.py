65df58690ac7d9f4ce6911c2b0d3296cb4043614
DrValkaryon <57381290+DrValkaryon@users.noreply.github.com>
2021-02-06 14:34:12 -0500

    Main.Py with accession function online.
    Retrieval from the NCBI servers is fully functional. Reports genbank, saves to local drive.

--- Genbanker/Main.py
+++ Genbanker/Main.py
@@ -1,4 +1,29 @@
 
-def Main:  
-val = input("Enter your accession: ") 
-print(val)
+import os
+from Bio import SeqIO
+from Bio import Entrez
+def Main():  
+    val = input("Enter your accession: ") 
+    print(val)
+    val2 = input("Enter your email: ")
+    GB_Hunter(val,val2) 
+
+
+def GB_Hunter (accession, email):
+    Entrez.email = email  # Always tell NCBI who you are
+    filename = accession + ".gb"
+    if not os.path.isfile(filename):
+        # Downloading...
+        net_handle = Entrez.efetch(
+            db="nucleotide", id=accession, rettype="gb", retmode="text"
+        )
+        out_handle = open(filename, "w")
+        out_handle.write(net_handle.read())
+        out_handle.close()
+        net_handle.close()
+        print("Saved")
+
+    print("Parsing...")
+    record = SeqIO.read(filename, "genbank")
+    print(record)
+Main()
